894665330605c1a88fe39e494b66fe6d709cbefd
koulanurag <koulanurag@gmail.com>
2023-06-09 00:51:06 -0400

    feat: init-commit

--- scripts/generate_stats.py
+++ scripts/generate_stats.py
@@ -1,3 +1,5 @@
+import pickle
+
 from sokoban_bazaar.dataset import get_dataset, pad_batch, _ENV_NAMES, _DATASET_NAMES
 from markdownTable import markdownTable
 import pandas as pd
@@ -6,7 +8,7 @@ import numpy as np
 
 
 def generate_dataset_stats():
-    df = pd.DataFrame(columns=['env_name', 'dataset_name', 'episodes'])
+    df = pd.DataFrame(columns=[])
     for env_name in _ENV_NAMES:
         for dataset_name in _DATASET_NAMES:
             try:
@@ -20,17 +22,27 @@ def generate_dataset_stats():
                 )
 
                 episode_score = {'max': -np.inf, 'min': np.inf, 'mean': 0}
+                episode_length = {'max': -np.inf, 'min': np.inf, 'mean': 0}
                 for batch in episode_dataloader:
                     episode_score['max'] = max(episode_score['max'], batch['returns_to_go'][:, 0, 0].max().item())
                     episode_score['min'] = min(episode_score['min'], batch['returns_to_go'][:, 0, 0].min().item())
                     episode_score['mean'] += batch['returns_to_go'][:, 0, 0].mean().item()
+
+                for file in episode_dataset.episode_files:
+                    _len = len(pickle.load(open(file, 'rb'))['returns_to_go'])
+                    episode_length['max'] = max(episode_length['max'], _len)
+                    episode_length['min'] = min(episode_length['min'], _len)
+                    episode_length['mean'] += _len
+
                 episode_score['mean'] /= len(episode_dataloader)
+                episode_length['mean'] /= len(episode_dataset.episode_files)
 
                 new_row = pd.Series({
                     'env_name': env_name,
                     'dataset_name': dataset_name,
                     'episodes': len(episode_dataset),
-                    **{f'{k}-score': v for k, v in episode_score.items()}
+                    **{f'{k}-score': v for k, v in episode_score.items()},
+                    **{f'{k}-len': v for k, v in episode_length.items()}
                 })
                 df = pd.concat([df, new_row.to_frame().T], ignore_index=True)
             except Exception as e:
