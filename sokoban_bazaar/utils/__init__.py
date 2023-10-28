7e4e4affd155b9c74be15f5d19afea29d63d9c8b
koulanurag <koulanurag@gmail.com>
2023-10-28 00:34:33 -0400

    fix: player position in set state

--- sokoban_bazaar/utils/__init__.py
+++ sokoban_bazaar/utils/__init__.py
@@ -14,7 +14,13 @@ def set_state(env, tiny_rgb_observation):
     env.unwrapped.room_fixed = info["room_fixed"]
     env.unwrapped.room_state = info["room_state"]
     env.unwrapped.box_mapping = info["box_mapping"]
-    env.unwrapped.player_position = np.argwhere(env.room_state == 5)[0]
+
+    # setup player location
+    player_position = np.argwhere(env.room_state == 5)  # player on floor cell
+    if len(player_position) == 0:
+        player_position = np.argwhere(env.room_state == 6)  # player on target
+    env.unwrapped.player_position = player_position[0]
+
     env.unwrapped.num_env_steps = 0
     env.unwrapped.reward_last = 0
     env.unwrapped.boxes_on_target = 0
