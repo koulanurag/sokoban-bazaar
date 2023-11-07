eae35df59ed8341d1f7d9e9015a68ca9800bb62e
koulanurag <koulanurag@gmail.com>
2023-11-07 00:25:17 -0500

    fix: init-commit

---
+++ scripts/compress_embedding_search_trajectories.py
@@ -0,0 +1,28 @@
+import pickle
+from pathlib import Path
+import os
+
+root_path = os.environ.get(
+    "SOKOBAN_DATASET_ROOT_PATH", default=os.path.join(Path.home(), ".sokoban-datasets")
+)
+
+
+def compress_search_trajectories():
+    env_name = "gym_sokoban:Boxoban-Train-v0"
+    dataset_name = "expert"
+    save_dir = os.path.join(
+        root_path, env_name, "tiny_rgb_array", dataset_name, "future-embedding"
+    )
+    os.makedirs(save_dir, exist_ok=True)
+
+    episodes = []
+    for file_name in os.listdir(save_dir):
+        if 'episode_' in file_name:
+            episode = pickle.load(open(os.path.join(save_dir, file_name), 'rb'))
+            episodes.append(episode)
+
+    pickle.dump(episodes, open(os.path.join(save_dir, f"trajectories.p"), "wb"))
+
+
+if __name__ == "__main__":
+    compress_search_trajectories()
